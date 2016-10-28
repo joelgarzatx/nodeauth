613695585c6214236fc104aa5e2a1db7125d9778
Joel Garza <joelgarzatx@users.noreply.github.com>
2016-10-28 07:56:58 -0500

    inital changes to fix Facebook, still broken

--- config/passport.js
+++ config/passport.js
@@ -121,15 +121,10 @@ module.exports = function(passport) {
     // =========================================================================
     // FACEBOOK ================================================================
     // =========================================================================
-    passport.use(new FacebookStrategy({
-
-        // pull in our app id and secret from our auth.js file
-        clientID        : configAuth.facebookAuth.clientID,
-        clientSecret    : configAuth.facebookAuth.clientSecret,
-        callbackURL     : configAuth.facebookAuth.callbackURL
-
-    },
-
+ 
+    var fbStrategy = configAuth.facebookAuth;
+    fbStrategy.passReqToCallback = true;  // allows us to pass in the req from our route (lets us check if a user is logged in or not)
+    passport.use(new FacebookStrategy(fbStrategy,
     // facebook will send back the token and profile
     function(token, refreshToken, profile, done) {
 
@@ -155,7 +150,7 @@ module.exports = function(passport) {
                     newUser.facebook.id    = profile.id; // set the users facebook id                   
                     newUser.facebook.token = token; // we will save the token that facebook provides to the user                    
                     newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
-                    newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+                    newUser.facebook.email = (profile.emails[0].value || '').toLowerCase(); // facebook can return multiple emails so we'll take the first
 
                     // save our user to the database
                     newUser.save(function(err) {
